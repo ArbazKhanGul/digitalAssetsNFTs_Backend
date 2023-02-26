d6d6cea632499b1d6b9a929f6d19a9f52f33a2aa
Arbaz Khan <arbazkhangul123@gmail.com>
2023-02-26 10:55:45 +0500

    notificationcomplete

--- src/controllers/individualnft.js
+++ src/controllers/individualnft.js
@@ -3,19 +3,21 @@ var NFT = require('../database/nftschema')
 exports.individualnft = async (req, res) => {
     let { nft_id } = req?.params;
     let user = req?.session?.user
-    console.log("🚀 ~ file: individualnft.js:4 ~ exports.individualnft= ~ user", user)
     console.log("🚀 ~ file: individualnft.js:3 ~ exports.collection= ~ nft_id", nft_id)
 
     let result = await NFT.findOne({ tokenURI: nft_id, status: { $ne: "notVerified" } }, {
         status: 1, price: 1,
         approved: 1, owner_address: 1, owner_email: 1,
         currentSellingId: 1,
-        tokenId:1
+        tokenId:1,
+        title:1,
+        nftName:1,
+        tokenURI:1
 
     });
+    console.log("🚀 ~ file: individualnft.js:15 ~ exports.individualnft= ~ result", result)
 
-    console.log("🚀 ~ file: individualnft.js:10 ~ exports.individualnft= ~ result", result)
-
+ 
     if (result) {
         res.send({ status: "success", nft: result, user: user });
         return;
