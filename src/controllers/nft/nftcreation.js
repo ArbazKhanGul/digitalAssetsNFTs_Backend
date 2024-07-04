143e9c04cd9284bbbe5d6530ff923800a679f83a
Arbaz Khan <arbazkhangul123@gmail.com>
2024-07-04 12:06:43 +0500

    error add

--- src/controllers/nft/nftcreation.js
+++ src/controllers/nft/nftcreation.js
@@ -4,61 +4,55 @@ var AdminData = require('../../database/adminData');
 const { ethers } = require("ethers");
 
 
-exports.nftVerification=async (req,res)=>{
+exports.nftVerification = async (req, res, next) => {
+  try {
+      const { nftName, hash, size, contentType } = req.body;
 
-  try{
-  const {nftName,hash,size,contentType} = req.body;
- 
-  const checkName = await NFT.findOne({nftName: nftName})
+      const checkName = await NFT.findOne({ nftName: nftName });
 
-  if(checkName){
+      if (checkName) {
+          if (checkName.status === "notVerified") {
+              let date = new Date();
+              let d1 = date.getTime();
+              let d2 = checkName.createdAt.getTime() + 600000;
 
-   if(checkName.status=="notVerified")
-   {
-   let date=new Date();
-   let d1=date.getTime();
+              if (d2 > d1) {
+                  res.send({ status: "timeNotPass", time: d2 - d1, type: "name" });
+                  return;
+              } else {
+                  await NFT.deleteOne({ _id: checkName._id });
+              }
+          } else {
+              res.send({ status: "duplicate", result: checkName, type: "name" });
+              return;
+          }
+      }
 
-   let d2=checkName.createdAt.getTime() + 600000;
+      const checkHash = await NFT.findOne({ hash: hash });
+      if (checkHash) {
+          res.send({ status: "duplicate", result: checkHash, type: "content" });
+          return;
+      }
 
-    if(d2 > d1){
-     res.send({ status: "timeNotPass",time:d2-d1,type:"name"})
-     return;
-    }
-    else{
-      await NFT.deleteOne({_id:checkName._id});
-    }
-   }
-   else{
-   res.send({ status: "duplicate",result:checkName,type:"name"})
-   return;
-   }
- }
+      let estimated_price_inDollar = 0;
+      if (contentType !== "image") {
+          estimated_price_inDollar = await bnb_price(size, contentType);
+      }
 
+      const result = await AdminData.findOne({});
+      const fee = ethers.utils.parseUnits(result.nftCreationFee.toString(), "ether");
 
- const checkHash = await NFT.findOne({hash: hash})
- if(checkHash){
-  res.send({ status: "duplicate",result:checkHash,type:"content"})
-   return;
- }
+      res.send({ status: "success", contentFee: estimated_price_inDollar, creationFee: fee });
+  } catch (error) {
+      console.error("🚀 ~ exports.nftVerification= ~ error:", error);
 
- let estimated_price_inDollar=0;
+      // Send error response with appropriate status code
+      res.status(500).send({ status: "error", message: error.message });
 
- if(contentType!="image"){
-  estimated_price_inDollar=await bnb_price(size,contentType);
- }
-
- const result = await AdminData.findOne({});
- const fee=ethers.utils.parseUnits(result.nftCreationFee.toString(), "ether");
-
- res.send({ status: "success",contentFee:estimated_price_inDollar,creationFee:fee})
-
+      // Alternatively, if you want to pass the error to the next middleware
+      // next(error);
   }
-  catch(error){
-    console.log("🚀 ~ exports.nftVerification= ~ error:", error)
-    throw new Error(error.message)
-  }
-
-}
+};
 
 
 exports.nftCreation = async (req, res) => {
